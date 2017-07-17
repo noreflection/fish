ae542635c9e308eefc3c197ee3ad4a6c269bf795
noreflection <a191a191a@gmail.com>
2017-07-17 03:18:41 +0300

    add post call from frontend

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -6,23 +6,37 @@ class AuthorsTable extends React.Component {
         super();
         this.state = {
             authors: [],
+            value:'',
             loading: true
         };
 
-        fetch("http://localhost:5001/api/test")
-            .then(response => response.json())
-            .then(data => this.setState({ authors: data, loading: false }))
+        this.getAuthors = this.getAuthors.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.addRandomAuthor = this.addRandomAuthor.bind(this);
     }
 
     render() {
         let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
+            ? <p><em></em></p>
             : this.renderAuthorsTable();
 
         return (
             <div>
-                <button id='get-button'>GET</button>
-                <button id='post-button'>POST</button>
+                <p>
+                    Author Name: <input
+                        type='text'
+                        id="author-name-input"
+                        placeholder='Author Name Placeholder'
+                        value={this.state.value}
+                        onChange={this.handleInputChange}>
+                    </input>
+                </p>
+                {/* <p>
+                    Status: <label>Status Placeholder</label>
+                </p> */}
+                <button id='add-author' onClick={this.addRandomAuthor}>ADD AUTHOR</button>
+                <button id='delete-author' onClick={this.deleteAuthor}>DELETE AUTHOR</button>
+                <button id='get-authors-list' onClick={this.getAuthors}>GET AUTHORS</button>
                 <p>{contents}</p>
             </div>);
     }
@@ -33,17 +47,48 @@ class AuthorsTable extends React.Component {
         return <table className='table'>
             <thead>
                 <tr>
-                    <th>Author's Names</th>
+                    <th>Author's id</th>
+                    <th>Author's Name</th>
                 </tr>
             </thead>
             <tbody>
                 {this.state.authors.map(author =>
-                    <tr key={author.id}>
+                    <tr key={author.authorId}>
+                        <td>{author.authorId}</td>
                         <td>{author.authorName}</td>
                     </tr>
                 )}
             </tbody>
         </table>;
+    }
+    handleInputChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    getAuthors() {
+        fetch("http://localhost:5001/api/test")
+            .then(response => response.json())
+            .then(data => this.setState({ authors: data, loading: false }))
+    }
+
+    addRandomAuthor() {
+        fetch("http://localhost:5001/api/test",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({ authorName: this.state.value, booksCount: 2 })
+            });
+
+        function makeid() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < 5; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            return text;
+        }
     }
 }
 export default AuthorsTable;
